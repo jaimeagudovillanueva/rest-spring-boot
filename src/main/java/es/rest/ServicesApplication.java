7a2795976c7672843aa12001228620c9c6225eb4
Jaime Agudo <jaime.agudo@externos.seap.minhap.es>
2017-05-12 08:34:20 +0200

    Autentication oauth2

--- src/main/java/es/rest/ServicesApplication.java
+++ src/main/java/es/rest/ServicesApplication.java
@@ -1,12 +1,13 @@
 package es.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.ldap.LdapAuthenticationProviderConfigurer;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
 
 /**
  * Clase principal
@@ -14,10 +15,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
  * @author jaime.agudo
  *
  */
-@Configuration
-@ComponentScan
-@EnableResourceServer
-@EnableAutoConfiguration
+@SpringBootApplication
 public class ServicesApplication extends SpringBootServletInitializer {
 
 	@Override
@@ -27,5 +25,33 @@ public class ServicesApplication extends SpringBootServletInitializer {
 
 	public static void main(final String[] args) {
 		SpringApplication.run(ServicesApplication.class, args);
+	}
+
+	@Autowired
+	public void authenticationManager(final AuthenticationManagerBuilder builder) throws Exception {
+
+		/*
+		 * builder.userDetailsService(new UserDetailsService() {
+		 *
+		 * @Override public UserDetails loadUserByUsername(final String s)
+		 * throws UsernameNotFoundException { final Usuario usuario = new
+		 * Usuario(); usuario.setUsername(s); usuario.setPassword("cambiame");
+		 * usuario.setActivo(true); return usuario; } });
+		 */
+
+		final DefaultSpringSecurityContextSource contextSource = new DefaultSpringSecurityContextSource(
+				"ldap://ldappre.map.es:389");
+		contextSource.setUserDn("cn=admin,dc=map,dc=es");
+		contextSource.setPassword("cambiame");
+		contextSource.setReferral("follow");
+		contextSource.setBase("dc=map,dc=es");
+		contextSource.afterPropertiesSet();
+
+		final LdapAuthenticationProviderConfigurer<AuthenticationManagerBuilder> ldapAuthenticationProviderConfigurer = builder
+				.ldapAuthentication();
+
+		ldapAuthenticationProviderConfigurer.userSearchFilter("uid={0}").userSearchBase("")
+				.contextSource(contextSource);
+
 	}
 }
